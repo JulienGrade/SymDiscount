623266ef9c44330f8b2c366612719d7fb246f4e3
JulienGrade <gradejulien@gmail.com>
2020-02-09 18:32:16 +0100

    Create Cart service and add cart system to /panier and buy option ok on /products

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,38 +2,55 @@
 
 namespace App\Controller;
 
+
+use App\Service\Cart\CartService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+use Symfony\Component\HttpFoundation\Response;
+
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
     /**
      * @Route("/panier", name="cart_index")
+     * @param CartService $cartService
+     * @return Response
      */
-    public function index()
+    public function index(CartService $cartService)
     {
-        return $this->render('cart/index.html.twig',  []);
+        return $this->render('cart/index.html.twig',  [
+            'items' => $cartService->getFullCart(),
+            'total' => $cartService->getTotal()
+        ]);
     }
 
     /**
      * @Route("/panier/add/{id}", name="cart_add")
      * @param $id
-     * @param Request $request
+     * @param CartService $cartService
+     * @return RedirectResponse
      */
-    public function add($id, Request $request) {
-        $session = $request->getSession();
+    public function add($id, CartService $cartService)
+    {
+        $cartService->add($id);
 
-        $panier = $session->get('panier', []);
+        return $this->redirectToRoute("cart_index");
+    }
 
-        if(!empty($panier[$id])){
-            $panier[$id]++;
-        }else {
-            $panier[$id] = 1;
-        }
+    /**
+     * Permet de supprimer un produit du panier
+     *
+     * @Route("/panier/remove/{id}", name="cart_remove")
+     * @param $id
+     * @param CartService $cartService
+     * @return RedirectResponse
+     */
+    public function remove($id, CartService $cartService)
+    {
+        $cartService->remove($id);
 
-        $session->set('panier', $panier);
-
-        dd($session->get('panier'));
+        return $this->redirectToRoute("cart_index");
     }
 }
